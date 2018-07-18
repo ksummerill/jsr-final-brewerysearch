cdb568ea77b96ba4c450912717f813b07fbbd431
Kaitlyn <kaitlyn.summerill@mapbox.com>
2018-07-18 12:21:34 -0700

    final project submission

--- Projects/jsr-final/jsr-final-project/BreweryMap.js
+++ Projects/jsr-final/jsr-final-project/BreweryMap.js
@@ -1,6 +1,5 @@
 // Beer Mapping API
 
-// desired url example: http://beermapping.com/webservice/locquery/cf14ac03008445d03cf9b26ed33bc258/goose+island&s=json
 var beerMappingUrl = 'http://beermapping.com/webservice/locquery/' + 'cf14ac03008445d03cf9b26ed33bc258/';
 var xhr = new XMLHttpRequest();
 
@@ -22,6 +21,7 @@ $(document).ready(function(){
     $.get(beerMappingUrl + "/" + $("#search").val() + "&s=json", function(data){
       console.log(data); // logs data from beermapping
 
+      // mostly for debugging purposes
       var searchValue = $("#search").val()
       console.log(searchValue)
       var breweryName = (data[0].name);
@@ -29,14 +29,19 @@ $(document).ready(function(){
       var breweryCity = (data[0].city);
       var breweryStreet = (data[0].street);
       var breweryScore = (data[0].overall);
+      var breweryUrl = (data[0].url);
       var place = (breweryName + breweryStreet + breweryCity + breweryState);
-      console.log(place); // logs info for Sierra Nevada in CA
+      console.log(place); // logs info for search
 
 
-    // Mapbox geocoder is giving me cities and state
-    $.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + searchValue + '.json?access_token=pk.eyJ1Ijoia3N1bW1lcmlsbCIsImEiOiJjajdreWRlNTgyaTl0MnFvMjRscnI1eDBvIn0.YzcP4_MZvzgm6HiMGcUsHQ&country=us&types=poi&autocomplete=true', function(data){
-        console.log($.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + searchValue + '.json?access_token=pk.eyJ1Ijoia3N1bW1lcmlsbCIsImEiOiJjajdreWRlNTgyaTl0MnFvMjRscnI1eDBvIn0.YzcP4_MZvzgm6HiMGcUsHQ')); //logs response from Mapbox
-        console.log(data.features.length); // logs coordinates for searchValue
+    // Mapbox geocoder
+    $.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + searchValue + '.json?access_token=pk.eyJ1Ijoia3N1bW1lcmlsbCIsImEiOiJjajdreWRlNTgyaTl0MnFvMjRscnI1eDBvIn0.YzcP4_MZvzgm6HiMGcUsHQ&country=us&types=poi&limit=1', function(data){
+        //console.log($.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + searchValue + '.json?access_token=pk.eyJ1Ijoia3N1bW1lcmlsbCIsImEiOiJjajdreWRlNTgyaTl0MnFvMjRscnI1eDBvIn0.YzcP4_MZvzgm6HiMGcUsHQ')); //logs response from Mapbox
+        console.log(data.features); // logs coordinates for searchValue
+
+        for (i = 0; i < data.features.length; i++);
+          var markerCoordinate = data.features[i];
+          console.log(markerCoordinate);
 
         // add markers to map
         data.features.forEach(function(marker) {
@@ -44,28 +49,48 @@ $(document).ready(function(){
         // create a HTML element for each feature
         var el = document.createElement('div');
         el.className = 'marker';
-        
 
-        // make a marker for each feature and add to the map
+        // make a marker for each feature and add to the map. coordinates are set by MB API
         new mapboxgl.Marker(el)
         .setLngLat(marker.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<h3>' + breweryName + '</h3><p>' + 'Score: ' + Math.round(breweryScore) + '</p>'))
+        .setPopup(new mapboxgl.Popup({closeButton: true, offset: 25 }) // add popups
+        .setHTML('<h3>' + breweryName + '</h3>' + '<h4>' + 'Score: ' + Math.round(breweryScore) + '<h4>' + '<p>' + 'Website: ' + breweryUrl + '</p>'))
         .addTo(map);
 
         // click on marker and fly to that location
-        $(".marker").click(function() {
-          var markerCoordinate = ([data.features.center]);
-          //var markerCoordinate = (marker.geometry.coordinates);
-          console.log(markerCoordinate);
+        el.addEventListener('click', function(e) {
+          flyToBrewery(marker);
+        });
+
+        function flyToBrewery(markerCoordinate) {
           map.flyTo({
-            center: markerCoordinate,
+            center: markerCoordinate.geometry.coordinates,
             zoom: 11
           });
-        });
+        }
       });
+
+      // click on x popup and fly back out to full map to search again
+
+      var el = document.createElement('div');
+        el.className = 'popup';
+        el.addEventListener('click', function(e) {
+          flyOut(popup);
+        });
+
+      var popup = new mapboxgl.Popup({closeButton: true})
+        popup.on('close', function(){
+         flyOut()
+        })
+
+      function flyOut() {
+        map.flyTo({
+          center: [-97.922211, 39.381266],
+          zoom: 3.5
+        });
+      }
     });
 
-  })
+    })
+  });
 });
-})
